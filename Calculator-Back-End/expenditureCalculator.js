caca53d7cdb069cf3514154fc836438c613fa045
thombator <thom.bator@gmail.com>
2023-03-21 13:09:30 -0400

    fixed a serious bug in the calculator that was leading to infinite loops, will continue testing but think it is good now

---
+++ Calculator-Back-End/expenditureCalculator.js
@@ -0,0 +1,3 @@
+const expenditureCalculator = (data) => {
+  const sortedPurchases = data.purchase.sort((a, b) => a.year - b.year);
+};
--- Calculator-Back-End/offsetCalculator.js
+++ Calculator-Back-End/offsetCalculator.js
@@ -1,29 +1,32 @@
 const offsetCalculator = (data) => {
   //Avg. CO 2 per person in tons must be converted to kg for comparison
-  const yearlyCarbon = data.country.carbon * 1000;
+  const yearlyCarbon = Number(data.country.carbon) * 1000;
   const countryName = data.country.name;
   //Sorting purchases in order of year in case they are submitted out of order
   //This will make the math easier
-  const sortedPurchases = data.purchase.sort((a, b) => a.year - b.year);
+  const sortedPurchases = data.purchase.sort(
+    (a, b) => Number(a.year) - Number(b.year)
+  );
   //Linear growth is assumed for carbon offsetting for simplicity
   //Tree is mature at 6 years. 28.5/6 - 4.75
   //Plan is to start at 4.75 in year 1 (i.e. 1 year after purchase).
   //Then will increment by 4.75 until trees are mature
   sortedPurchases.forEach((purchase) => (purchase.offset = 4.75));
-  sortedPurchases.forEach((purchase) => console.log(purchase));
+  console.log(sortedPurchases);
   //Increments current year being simulated
-  let currentYear = +sortedPurchases[0].year;
+  let currentYear = Number(sortedPurchases[0].year);
+  console.log("current year", currentYear);
   //Keeps count of total years to neutrality
   let years = 1;
   let yearlyOffset = 0;
   let isCarbonNeutral = false;
   const offsetsByYear = [];
 
-  while (yearlyOffset < yearlyCarbon) {
+  while (yearlyOffset < yearlyCarbon && years < sortedPurchases.length + 6) {
     yearlyOffset = 0;
     let allTreesMature = true;
     for (let purchase of sortedPurchases) {
-      if (purchase.year > currentYear) {
+      if (Number(purchase.year) > currentYear) {
         break;
       }
       yearlyOffset += +purchase.trees * purchase.offset;
@@ -33,7 +36,9 @@ const offsetCalculator = (data) => {
       }
     }
     //forLoopEnded
-    const offsetsByYearObj = { currentYear: yearlyOffset };
+
+    const offsetsByYearObj = { currentYear, yearlyOffset };
+    console.log(offsetsByYearObj);
     offsetsByYear.push(offsetsByYearObj);
 
     if (yearlyOffset >= yearlyCarbon) {
